59be68108c8afa36d0154a88913c4c8c8fafb707
Prashanth Ramakrishnan <prashanth.ramakrishnan@zalando.de>
2019-03-09 12:05:33 +0100

    repo first commit

---
+++ src/main/kotlin/com/tafel/explorer/tafelexplorer/TafelExplorerApplication.kt
@@ -0,0 +1,11 @@
+package com.tafel.explorer.tafelexplorer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TafelExplorerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TafelExplorerApplication>(*args)
+}
---
+++ src/test/kotlin/com/tafel/explorer/tafelexplorer/TafelExplorerApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.tafel.explorer.tafelexplorer
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TafelExplorerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
